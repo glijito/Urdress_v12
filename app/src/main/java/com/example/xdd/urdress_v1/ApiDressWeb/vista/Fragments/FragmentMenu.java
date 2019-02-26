56db77cd6df29b38d94c7fe2c948125c3bab2e18
marcoabonce <43801901+marcoabonce@users.noreply.github.com>
2019-02-26 10:31:35 -0600

    Inicio, perfil, publicados

---
+++ app/src/main/java/com/example/xdd/urdress_v1/ApiDressWeb/vista/Fragments/FragmentMenu.java
@@ -0,0 +1,91 @@
+package com.example.xdd.urdress_v1.ApiDressWeb.vista.Fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.xdd.urdress_v1.R;
+
+public class FragmentMenu extends Fragment {
+
+    private DataListener callback;
+    private TextView Perfil;
+    private TextView Publicados;
+    private TextView Rentados;
+    private TextView Terminos;
+    private TextView AcercaDe;
+
+    public FragmentMenu() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment__menu, container, false);
+        try {
+            callback = (DataListener) getActivity();
+        } catch (Exception e) {
+            throw new ClassCastException(getActivity().toString() + " should implement DataListener");
+        }
+
+        Perfil = (TextView) view.findViewById(R.id.Perfil);
+        Publicados = (TextView) view.findViewById(R.id.Publicados);
+        Rentados = (TextView) view.findViewById(R.id.rentados);
+        Terminos = (TextView) view.findViewById(R.id.terminos);
+        AcercaDe = (TextView) view.findViewById(R.id.acerca);
+
+        Perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.sendMenu("Perfil");
+            }
+        });
+        Publicados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.sendMenu("Publicados");
+            }
+        });
+        Rentados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.sendMenu("Rentados");
+            }
+        });
+        Terminos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.sendMenu("Terminos");
+            }
+        });
+        AcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.sendMenu("AcercaDe");
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    public interface DataListener{
+        void sendMenu(String fragmento);
+    }
+
+}
---
+++ app/src/main/java/com/example/xdd/urdress_v1/ApiDressWeb/vista/Fragments/FragmentPublicados.java
@@ -0,0 +1,30 @@
+package com.example.xdd.urdress_v1.ApiDressWeb.vista.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.xdd.urdress_v1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentPublicados extends Fragment {
+
+
+    public FragmentPublicados() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_publicados, container, false);
+    }
+
+}
